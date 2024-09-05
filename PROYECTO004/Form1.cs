f171ffe79c52d423a1af11b9e74bcd8cc9cdcadc
Mansa <luis.sanchez77@unach.mx>
2024-09-05 12:52:35 -0600

    Version1

--- PROYECTO004/Form1.cs
+++ PROYECTO004/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PROYECTO004
 {
@@ -43,15 +44,36 @@ namespace PROYECTO004
                 genero = "Femenino";
             }
             //creacion de la cadena de datos
-            string datos = $"nombres: {nombres}\r\nApellidos:{apellido}";
+            string datos = $"Nombres: {nombres}\r\nApellidos: {apellido}\r\nTelefono: {telefono}\r\nEstatura: {estatura}\r\nEdad: {edad}";
 
             //guaardar en el txtx
             string savefile = "C:/Users/Mansa/OneDrive/Documentos/UNACH/TERCER SEMESTRE/PROGRAMACION AVANZADA/DATOS FORM/DATOS.TXT";
+            //verificar si el arhcivo existe
+            bool Chekfile = File.Exists(savefile);
+
+            using (StreamWriter writer = new StreamWriter(savefile,true))
+            {
+                if (Chekfile)
+                {
+                    writer.WriteLine("+-+-+-+-+-+-+-+-+-+-+");
+                }
+                writer.WriteLine(datos);
+                //TEXT BOX DESPUES DEL GUARDADO
+                MessageBox.Show("INFORMACION GUARDADA: \n\n" + datos, "INFORMACION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                //TITULO, CADENA, TITULO DE LA VENTANA, BOTON DE OK AUTOGENERADO, ICONO DE LA VENATANA            
+            }
 
         }
 
         private void bt_Limpiar_Click(object sender, EventArgs e)
         {
+            tbNombre.Clear();
+            tbApellidos.Clear();
+            tbTelefono.Clear();
+            tbEstatura.Clear();
+            tbEdad.Clear();
+            rB_Femenino.Checked = false;
+            rB_Masculino.Checked = false;
 
         }
     }
